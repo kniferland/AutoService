bbbbfa34666ab6fe74f3920ec9f3c61d03b55afd
kniferland <kniferland@gmail.com>
2020-12-14 14:45:18 +0300

    11

--- AutoService/Views/Windows/AdminWindow.xaml.cs
+++ AutoService/Views/Windows/AdminWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AutoService.Utils;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,6 +12,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace AutoService.Views.Windows
 {
@@ -19,14 +21,26 @@ namespace AutoService.Views.Windows
     /// </summary>
     public partial class AdminWindow : Window
     {
+        DispatcherTimer timer;  // объявляем переменную таймера для обновления экрана
         public AdminWindow()
         {
             InitializeComponent();
+            timer = new DispatcherTimer();//обновление страницы
+            timer.Interval = new TimeSpan(0, 0, 30);//таймер
+            timer.Tick += Timer_Tick; //новый метод
+            timer.Start(); //запускаем таймер
         }
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
+            DateTime nextDay = DateTime.Now.AddDays(1.0);
+            ClientGrid.ItemsSource = AppData.db.ClientServices.Where(c => c.StartTime >= DateTime.Now && c.StartTime <= nextDay).OrderBy(c => c.StartTime).ToList(); //и сортировка
+        }
 
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            DateTime nextDay = DateTime.Now.AddDays(1.0); //переменная текущий день + 1 день
+            ClientGrid.ItemsSource = AppData.db.ClientServices.Where(c => c.StartTime >= DateTime.Now && c.StartTime <= nextDay).OrderBy(c => c.StartTime).ToList(); //и сортировка
         }
     }
 }
